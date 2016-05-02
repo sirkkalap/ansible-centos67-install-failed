75e2d490ed4a35a03d47178fae76510788c7e80f
Petri Sirkkala <sirpete@iki.fi>
2016-05-02 17:42:45 +0300

    Cleared up instructions

--- runme.sh
+++ runme.sh
@@ -7,5 +7,5 @@ img=$(docker build -q .)
 echo "Running pip install ansible"
 docker run -i -t ${img}
 
-echo "Running bash without pip install ansible"
+echo "Running bash without pip install ansible. To repeat, run 'pip install ansible'."
 docker run -i -t ${img} bash

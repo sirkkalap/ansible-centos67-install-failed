ab8cb084e8905385470c8e7366125f6fa0197242
Petri Sirkkala <sirpete@iki.fi>
2016-05-02 17:37:53 +0300

    Quiet option to build to get just the img name

--- runme.sh
+++ runme.sh
@@ -2,10 +2,10 @@
 # Repeatable failure with pip install ansible on Centos 6.7
 
 echo "Building image"
-img=$(docker build .)
+img=$(docker build -q .)
 
 echo "Running pip install ansible"
-docker run -i -t $img
+docker run -i -t ${img}
 
 echo "Running bash without pip install ansible"
-docker run -i -t $img bash
+docker run -i -t ${img} bash
